9768550b30750c2963cf84f4a972d1d9b9a58d7f
Jose Granado <jfgracol@gmail.com>
2025-12-10 17:27:16 -0400

    implementacion del main(App.java)

--- minijuego/minijuego_snake/src/App.java
+++ minijuego/minijuego_snake/src/App.java
@@ -1,5 +1,18 @@
+import controller.GameController;
+import model.GameModel;
+import view.GameFrame;
+import javax.swing.SwingUtilities;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                GameModel model = new GameModel();
+                GameFrame view = new GameFrame(model);
+                GameController controller = new GameController(model, view);
+                controller.start();
+            }
+        });
     }
 }
